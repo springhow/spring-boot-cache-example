4118da4d92db253f5badf9c733e3df04e73d78de
Raja Anbazhagan <raja.anbazhagan1991@gmail.com>
2020-12-13 00:17:24 +0530

    Initial commit

---
+++ src/main/java/com/springhow/examples/springboot/cache/entities/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.springhow.examples.springboot.cache.entities;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+}
